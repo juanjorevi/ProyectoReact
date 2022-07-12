3a0fe87fb4a8a6352db172311ae1c880950f9c80
juanjo <jjreviglio@gmail.com>
2022-07-12 08:57:47 -0300

    trabajo 8

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 import "./ItemDetail.css";
 import { Link } from "react-router-dom";
+import ItemContador from "../ItemListContador";
 
 const ItemDetail =({Data})=>{
+    const [gotoCart,setGoToCart]=useState(false)
+
+    const onAdd =(valor)=>{
+        setGoToCart(true)
+        console.log(`compraste ${valor} unidades`);  
+      }
     return(
         <div className="container">
             <div className=" row detail">
                 <div className="  detail_image ">
-                 <img src={Data.imgUrl} className="image col-xs-12 col-sm-6 col-lg-6" alt={Data.nombre} />
-                 <p className="descripcion col-xs-12 col-sm-6 col-lg-6">descripcion:{Data.descripcion}</p>
+                 <img src={Data.imgUrl} className="image col-xs-12 col-sm-6 col-lg-4" alt={Data.nombre} />
+                 <p className="descripcion col-xs-12 col-sm-6 col-lg-4">descripcion:{Data.descripcion}</p>
+                 {
+                    gotoCart
+                    ?<Link to="/cart">terminar seleccion de pokemon</Link>
+                    : <ItemContador inicial={1} stock={6} onAdd={onAdd}/>
+
+                 }
                 </div>
                 <h3 className="name">{Data.nombre}</h3>
                 <p className="caracteristica">TIPO:{Data.tipo}</p>
